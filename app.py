c660fd5e28f3fd0a3fe5cc8dc0abae37a6508fa5
bill10 <billanson10@gmail.com>
2023-12-29 10:33:36 -0800

    Start uber tutorial

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import streamlit as st
-import time
+import pandas as pd
+import numpy as np
 
-'Starting a long computation...'
+st.title('Uber pickups in NYC')
 
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
-'...and now we\'re done!'
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...done!')
